39847f692d4ed85e75a612e3ef8f96927bb721ed
Francisco Javier Diaz Martin <fjdiaz@stratio.com>
2018-11-27 14:09:08 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Pokemon from "./Pokemon";
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    showPokemonDescription: false
+  };
+  showPokemonDescription = () => {
+    this.setState({
+      showPokemonDescription: true
+    });
+  };
   render() {
+    const { showPokemonDescription } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {!showPokemonDescription && (
+          <button onClick={this.showPokemonDescription}>
+            Show Charmander Description
+          </button>
+        )}
+        {showPokemonDescription && <Pokemon />}
       </div>
     );
   }
 }
-
 export default App;
